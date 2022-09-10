0ad51ec885bca3e9cdab99507af28e1b1175e4e8
Todd Kirby <ffmpeg.php@gmail.com>
2022-09-11 02:57:38 +0700

    Add Swift Package Manager support.

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version: 5.6
+
+import PackageDescription
+
+let package = Package(
+    name: "MMSCameraViewController",
+    platforms: [
+        .iOS(.v9)
+    ],
+    products: [
+        .library(
+            name: "MMSCameraViewController",
+            targets: ["MMSCameraViewController"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "MMSCameraViewController",
+            dependencies: [],
+            path: "MMSCameraViewController/Classes/",
+            publicHeadersPath: ".",
+            linkerSettings: [
+                //Frameworks
+            .linkedFramework("AVFoundation"),
+            .linkedFramework("UIKit"),
+            .linkedFramework("Foundation"),
+            ]),
+        .testTarget(
+            name: "MMSCameraViewControllerTests",
+            dependencies: ["MMSCameraViewController"]),
+    ]
+)
